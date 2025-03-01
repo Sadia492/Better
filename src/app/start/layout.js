de0ee2bca780189ffd5d44f4d5f2dc7b1b045b86
Sadia492 <sadiaafrin2792>
2025-03-01 20:12:03 +0600

    favicon replaced

--- src/app/start/layout.js
+++ src/app/start/layout.js
@@ -1,11 +1,15 @@
 import Footer from "@/components/Footer";
 import StartNav from "@/components/StartNav";
 import "../globals.css";
+import Head from "next/head";
 
 // src/app/start/layout.js
 export default function StartLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body>
         <div className="bg-[#FEFCFA]">
           <StartNav /> {/* Custom Navbar for Start Page */}
